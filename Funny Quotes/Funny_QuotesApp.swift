643381de0507ee5503e112d43f7b075ee0c06b6e
John Pillsbury <jpillsbu@gmail.com>
2021-03-25 19:06:40 -0400

    Initial Commit

---
+++ Funny Quotes/Funny_QuotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Funny_QuotesApp.swift
+//  Funny Quotes
+//
+//  Created by John Pillsbury on 3/25/21.
+//
+
+import SwiftUI
+
+@main
+struct Funny_QuotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
